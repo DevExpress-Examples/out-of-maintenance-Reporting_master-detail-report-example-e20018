0dce278c3c33f97534957f066f908036843bddf1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Users/Reports/UserItemReport.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Users.Reports
+{
+    public partial class UserItemReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public UserItemReport()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Master-Detail Report Example
+
+
+<p>This example demonstrates multiple detail bands in a master-detail report.</p>
+
+<br/>
+
+
